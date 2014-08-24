d25f0a2d8a1a289b2c1c3534bd00b1c6873af34a
Jonathan Wesley Stone <js@piratejon.com>
2014-08-24 12:15:28 -0500

    added some programs, doing constants also so we can do lda

--- pmachine.js
+++ pmachine.js
@@ -7,6 +7,7 @@ var pmachine = (function () {
 
     function datastore_push(id, type, value) {
         G.dstore.push({id: id, type: type, value: value});
+        G.R.sp = G.dstore.length - 1;
     }
 
     function datastore_pop() {
@@ -15,21 +16,54 @@ var pmachine = (function () {
         return popee;
     }
 
-    function base(level) {
-        return parseInt(level, 10);
+    function get_frame_pointer(mp, v) {
+        var offset_map = {
+            "rv": 0,
+            "sl": 1,
+            "dl": 2,
+            "ep": 3,
+            "ra": 4
+        };
+
+        return mp + offset_map[v];
+    }
+
+    // try to resolve l into a label or assume it's an address
+    function int_from_label(l) {
+        if (G.line_labels.hasOwnProperty(l)) {
+            return parseInt(G.line_labels[l], 10);
+        }
+
+        if (G.data_labels.hasOwnProperty(l)) {
+            return parseInt(G.data_labels[l], 10);
+        }
+
+        return parseInt(l, 10);
+    }
+
+    function follow_link(level, mp, frame_element) {
+        if (level === 0) {
+            return mp;
+        }
+
+        return follow_link(level - 1, G.dstore[get_frame_pointer(mp, frame_element)], frame_element);
     }
 
     function insn_mst(g, insn) {
         datastore_push("rv", "int", 0);
-        datastore_push("sl", "int", base(insn.op1));
-        datastore_push("dl", "int", 0);
+        datastore_push("sl", "int", follow_link(parseInt(insn.op1, 10), g.R.mp, "dl"));
+        datastore_push("dl", "int", g.R.mp); // the dynamic link points to the callee's stack frame
         datastore_push("ep", "int", g.R.ep);
 
+        g.R.mp = g.R.ep;
+        g.R.sp += 5;
         g.R.pc += 1;
     }
 
     function insn_cup(g, insn) {
         datastore_push("ra", "int", g.R.pc + 1);
+
+        g.R.mp = g.R.sp - (parseInt(insn.op1, 10) + 4);
 
         if (g.line_labels.hasOwnProperty(insn.op2)) {
             g.R.pc = g.line_labels[insn.op2];
@@ -42,10 +76,26 @@ var pmachine = (function () {
         g.form.step.disabled = true;
     }
 
-    /*
     function insn_ent(g, insn) {
+        if (insn.op1 === "sp") {
+            g.R.sp = g.R.mp + int_from_label(insn.op2);
+        } else if (insn.op1 === "ep") {
+            g.R.ep = g.R.mp + int_from_label(insn.op2);
+        }
+
+        g.R.pc += 1;
     }
 
+    function insn_rtn(g, insn) {
+        g.R.pc = datastore_pop().value;
+    }
+
+    function insn_lda(g, insn) {
+        // possibly overwrites parts of the stack frame!
+        datastore_push("", "int", follow_link(parseInt(insn.op1, 10), g.R.mp, "dl") + parseInt(insn.op2, 10));
+    }
+
+    /*
     function insn_lvi(g, insn) {
     }
 
@@ -53,12 +103,6 @@ var pmachine = (function () {
     }
 
     function insn_ldc(g, insn) {
-    }
-
-    function insn_rtn(g, insn) {
-    }
-
-    function insn_lda(g, insn) {
     }
 
     function insn_mod(g, insn) {
@@ -88,12 +132,14 @@ var pmachine = (function () {
         G.line_labels_rev = {};
         G.data_labels = {};
         G.data_labels_rev = {};
+        G.constants = [];
         G.implicit_label_matcher = /^(L\d+)/;
         G.explicit_label_matcher = /^#define[ \t]+(L\d+)[ \t]+(\d+)/;
-        G.instruction_matcher = /^([A-Za-z0-9]{0,10})?[ \t]*([A-Za-z0-9]{0,10})?[ \t]*([A-Za-z0-9]{0,10})?[ \t]*([A-Za-z0-9]*)?[ \t]*$/;
+        G.instruction_matcher = /^([A-Za-z0-9]{0,10})?[ \t]*([A-Za-z0-9]{0,10})?[ \t]*([A-Za-z0-9]{0,10})?[ \t]*([A-Za-z0-9' \t]*)/;
         // G.log = console.log;
         G.log = function () { };
         G.form = {
+            constants: document.getElementById('constants_body'),
             labels: document.getElementById('label_body'),
             istore: document.getElementById('istore_body'),
             dstore: document.getElementById('dstore_body'),
@@ -103,14 +149,14 @@ var pmachine = (function () {
 
         G.opcode_dispatch = {
             "mst": insn_mst,
-            "cup": insn_cup/*,
+            "cup": insn_cup,
             "stp": insn_stp,
             "ent": insn_ent,
+            "rtn": insn_rtn,
+            "lda": insn_lda/*
             "lvi": insn_lvi,
             "csp": insn_csp,
             "ldc": insn_ldc,
-            "rtn": insn_rtn,
-            "lda": insn_lda,
             "mod": insn_mod,
             "sti": insn_sti,
             "equ": insn_equ,
@@ -144,15 +190,31 @@ var pmachine = (function () {
         G.log("Stored data label " + scan[1] + "->" + scan[2]);
     }
 
+    function insert_constant(insn) {
+        var l = G.constants.length;
+        G.constants.push({type: insn.op1, value: insn.op2});
+        return l.toString();
+    }
+
     function scan_instruction(i, line) {
-        var scan = G.instruction_matcher.exec(line);
-        return {
+        var insn, scan;
+
+        scan = G.instruction_matcher.exec(line);
+
+        insn = {
             address: i,
             label: scan[1],
             opcode: scan[2],
             op1: scan[3],
-            op2: scan[4]
+            op2: scan[4].trim()
         };
+
+        // check for and insert any constants found
+        if (insn.opcode === "ldc" && insn.op1 !== "c" && insn.op1 !== "b") {
+            insn.op2 = insert_constant(insn);
+        }
+
+        return insn;
     }
 
     function instruction_array_from_pcode(pcode) {
@@ -248,7 +310,8 @@ var pmachine = (function () {
             }
         }
 
-        return -1;
+        // if there is no mark stack just start at the top
+        return 0;
     }
 
     function render_labels(g) {
@@ -280,6 +343,30 @@ var pmachine = (function () {
             if (g.data_labels.hasOwnProperty(label)) {
                 g.form.labels.appendChild(wrap(label, g.data_labels[label]));
             }
+        }
+    }
+
+    function render_constants(g) {
+        var wrap, i;
+
+        wrap = function (l, v) {
+            var tr, td;
+
+            tr = document.createElement('tr');
+
+            td = document.createElement('td');
+            td.innerHTML = l;
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerHTML = v;
+            tr.appendChild(td);
+
+            return tr;
+        };
+
+        for (i = 0; i < g.constants.length; i += 1) {
+            g.form.constants.appendChild(wrap(i, g.constants[i]));
         }
     }
 
@@ -333,6 +420,7 @@ var pmachine = (function () {
     function render_static_visual_elements(g) {
         render_labels(g);
         render_static_istore_elements(g);
+        render_constants(g);
         // render_static_dstore_elements(g);
     }
 
@@ -343,9 +431,11 @@ var pmachine = (function () {
     }
 
     function reset() {
-        init();
+        G.dstore = [];
+        G.constants = [];
 
         G.istore = instruction_array_from_pcode(G.form.pcode.value.split('\n'));
+
         initialize_registers(G);
 
         reset_visual_elements(G);
@@ -387,12 +477,16 @@ var pmachine = (function () {
 
         insn = G.istore[G.R.pc];
 
-        console.log('"' + insn.opcode + '"');
+        console.log("Executing " + insn);
         G.opcode_dispatch[insn.opcode](G, insn);
 
         render_dynamic_visual_elements(G);
     }
 
-    return { 'init': init, 'reset': reset, 'step': step };
+    function new_program() {
+        G.form.pcode.value = document.getElementById('installed_programs').value;
+    }
+
+    return { 'init': init, 'reset': reset, 'step': step, 'new_program': new_program };
 }());
 
