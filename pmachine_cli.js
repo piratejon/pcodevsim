2c3b082cc849aaeed6159815184fbde6d1a1bc44
Jonathan Wesley Stone <js@piratejon.com>
2014-09-01 17:16:26 -0500

    well it helps when you refer to the right variables in their proper namespaces

--- pmachine_cli.js
+++ pmachine_cli.js
@@ -7,10 +7,12 @@ var program_text = fs.readFileSync(process.argv[2], "utf8");
 pmachine.init();
 pmachine.reset(program_text.split('\n'));
 
+var stdout = pmachine.stdout();
+
 while (pmachine.state() === "running") {
     pmachine.step();
-    while (pmachine.stdout.length > 0) {
-        process.stdout.write(stdout[0]);
+    while (stdout.length > 0) {
+        console.log(stdout[0]);
         stdout.shift();
     }
 }
