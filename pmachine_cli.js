4978352fa5610e0d4c9f4d41ec25b8a674d021db
Jonathan Wesley Stone <js@piratejon.com>
2014-09-30 18:10:35 -0500

    somehow pmachine.js got chopped off?

--- pmachine_cli.js
+++ pmachine_cli.js
@@ -1,3 +1,4 @@
+"use strict";
 
 var fs = require('fs');
 var pmachine = require('./pmachine.js').pmachine;
@@ -7,13 +8,27 @@ var program_text = fs.readFileSync(process.argv[2], "utf8");
 pmachine.init();
 pmachine.reset(program_text.split('\n'));
 
-var stdout = pmachine.stdout_buffer();
+var pmachine_stdout = pmachine.stdout_buffer();
+pmachine.set_stdin_callback(function () {
+    var len, buffer;
+    len = 100;
+    buffer = new Buffer(len);
+    fs.readSync(process.stdin.fd, buffer, 0, len);
+    return buffer;
+});
+
+process.stdin.resume();
 
 while (pmachine.state() === "running") {
     pmachine.step();
-    while (stdout.length > 0) {
-        console.log(stdout[0]);
-        stdout.shift();
+
+    while (pmachine_stdout.length > 0) {
+        process.stdout.write(pmachine_stdout[0]);
+        pmachine_stdout.shift();
     }
 }
+
+process.stdout.write("Program terminated.");
+
+process.stdin.pause();
 
