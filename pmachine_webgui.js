44da4bf60e97785bb58a65481fbf8583d08d139c
Jonathan Wesley Stone <js@piratejon.com>
2014-10-17 20:44:40 -0500

    things are improving but not quite there. stdout?!?!

--- pmachine_webgui.js
+++ pmachine_webgui.js
@@ -19,7 +19,7 @@ var pmachine_webgui = (function () {
         gui.stdout.value = '';
     }
 
-    function render_static_visual_elements(gui, machine_state) {
+    function render_static_istore_elements(gui, machine_state) {
         var insn, insn_table_row;
 
         insn_table_row = function (insn) {
@@ -48,7 +48,7 @@ var pmachine_webgui = (function () {
         }
     }
 
-    function render_dynamic_visual_elements(gui, machine_state) {
+    function render_dynamic_istore_elements(gui, machine_state) {
         if (machine_state.R.pc !== -1) {
             if (old_state !== undefined && old_state.R.pc !== -1) {
                 gui.istore.childNodes[old_state.R.pc].removeAttribute('id');
@@ -57,12 +57,156 @@ var pmachine_webgui = (function () {
         }
     }
 
+    function render_labels(gui, machine_state) {
+        var wrap, label;
+
+        wrap = function (l, v) {
+            var tr, td;
+
+            tr = document.createElement('tr');
+
+            td = document.createElement('td');
+            td.innerHTML = l;
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerHTML = v;
+            tr.appendChild(td);
+
+            return tr;
+        };
+
+        for (label in machine_state.line_labels) {
+            if (machine_state.line_labels.hasOwnProperty(label)) {
+                gui.labels.appendChild(wrap(label, machine_state.line_labels[label]));
+            }
+        }
+
+        for (label in machine_state.data_labels) {
+            if (machine_state.data_labels.hasOwnProperty(label)) {
+                gui.labels.appendChild(wrap(label, machine_state.data_labels[label]));
+            }
+        }
+    }
+
+    function render_constants(gui, machine_state) {
+        var wrap, i;
+
+        wrap = function (l, v) {
+            var tr, td;
+
+            tr = document.createElement('tr');
+
+            td = document.createElement('td');
+            td.innerHTML = l;
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerHTML = v.type;
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerHTML = v.value;
+            tr.appendChild(td);
+
+            return tr;
+        };
+
+        for (i = 0; i < machine_state.constants.length; i += 1) {
+            gui.constants.appendChild(wrap(i, machine_state.constants[i]));
+        }
+    }
+
+    function render_static_visual_elements(gui, machine_state) {
+        render_labels(gui, machine_state);
+        render_static_istore_elements(gui, machine_state);
+        render_constants(gui, machine_state);
+    }
+
+    function render_dynamic_dstore_elements(gui, machine_state, old_state) {
+        var cell, wrap;
+
+        clear_children(gui.dstore);
+
+        wrap = function (machine_state, address) {
+            var tr, wrap2, c, o;
+
+            c = machine_state.dstore[address];
+            o = old_state.dstore[address] || {};
+
+            wrap2 = function (d, old) {
+                var td = document.createElement('td');
+                td.innerHTML = d;
+                if (d !== old) {
+                    td.classList.add('just_changed');
+                }
+                return td;
+            };
+
+            tr = document.createElement('tr');
+            tr.appendChild(wrap2(address, address));
+            tr.appendChild(wrap2(c.id, o.id));
+            tr.appendChild(wrap2(c.type, o.type));
+            tr.appendChild(wrap2(c.value, o.value));
+
+            if (address === machine_state.R.sp) {
+                tr.id = 'sp_row';
+            }
+
+            if (address === machine_state.R.ep) {
+                tr.id = 'ep_row';
+            }
+
+            if (address === machine_state.R.mp) {
+                tr.id = 'mp_row';
+            }
+
+            return tr;
+        };
+
+        for (cell = machine_state.dstore.length - 1; cell >= 0; cell -= 1) {
+            gui.dstore.appendChild(wrap(machine_state, cell));
+        }
+    }
+
+    function render_registers(gui, machine_state) {
+        var reg, elt;
+        for (reg in machine_state.R) {
+            if (machine_state.R.hasOwnProperty(reg)) {
+                elt = gui.registers[reg];
+                elt.innerHTML = machine_state.R[reg];
+                if (machine_state.R[reg] === old_state.R[reg]) {
+                    // http://stackoverflow.com/questions/195951/change-an-elements-css-class-with-javascript
+                    elt.classList.remove('just_changed');
+                } else {
+                    elt.classList.add('just_changed');
+                }
+            }
+        }
+    }
+
+    function append_stdout(gui, machine_state) {
+        var pmachine_stdout = machine_state.stdout_buffer;
+        while (pmachine_stdout.length > 0) {
+            gui.stdout.value += pmachine_stdout[0];
+            pmachine_stdout.shift();
+        }
+    }
+
+    function render_dynamic_visual_elements(gui, machine_state, old_state) {
+        render_registers(gui, machine_state);
+        render_dynamic_istore_elements(gui, machine_state);
+        render_dynamic_dstore_elements(gui, machine_state, old_state);
+        append_stdout(gui, machine_state);
+    }
+
     function reset() {
+        var machine_state = pm.get_machine_state();
         pm.init();
         pm.reset(gui.program_text.value.split('\n'));
         reset_visual_elements(gui);
-        render_static_visual_elements(gui, pm.get_machine_state());
-        render_dynamic_visual_elements(gui, pm.get_machine_state());
+        render_static_visual_elements(gui, machine_state);
+        render_dynamic_visual_elements(gui, machine_state, old_state);
     }
 
     function pmachine_loaded() {
@@ -154,7 +298,15 @@ var pmachine_webgui = (function () {
             istore: document.getElementById('istore_body'),
             dstore: document.getElementById('dstore_body'),
             pcode: document.getElementById('pcode'),
-            step: document.getElementById('step')
+            step: document.getElementById('step'),
+
+            registers: {
+                'pc': document.getElementById('val_pc'),
+                'sp': document.getElementById('val_sp'),
+                'mp': document.getElementById('val_mp'),
+                'np': document.getElementById('val_np'),
+                'ep': document.getElementById('val_ep'),
+            },
         };
         return new_gui;
     }
@@ -166,9 +318,19 @@ var pmachine_webgui = (function () {
         g.program_text.value = document.getElementById('installed_programs').value;
     }
 
+    function initialize_old_state() {
+        return {
+            'dstore': [],
+            'R': {
+                'pc': -1,
+            },
+        };
+    }
+
     function bodyload() {
         document.getElementById('installed_programs').children[0].selected = 'selected';
         gui = initialize_gui_bits();
+        old_state = initialize_old_state();
         load_pmachine();
     }
 
